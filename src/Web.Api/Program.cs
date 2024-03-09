be29f20edfeb3ea9bfce3e30bb407d631a6c2633
Fernando Ventura <ferventurart@gmail.com>
2024-03-09 12:23:47 -0500

    🏗️ Adding docker environment
    Adding docker environment for entire application

---
+++ src/Web.Api/Program.cs
@@ -0,0 +1,56 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Serilog;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfig) =>
+    loggerConfig.ReadFrom.Configuration(context.Configuration));
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+string connectionString = builder.Configuration.GetConnectionString("Database");
+Ensure.NotNullOrEmpty(connectionString);
+string redisConnectionString = builder.Configuration.GetConnectionString("Cache");
+Ensure.NotNullOrEmpty(redisConnectionString);
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(connectionString)
+    .AddRedis(redisConnectionString);
+
+builder.Services.AddStackExchangeRedisCache(options =>
+    options.Configuration = redisConnectionString);
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapHealthChecks("health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+
+app.UseRequestContextLogging();
+
+app.UseSerilogRequestLogging();
+
+app.UseExceptionHandler();
+
+app.Run();
